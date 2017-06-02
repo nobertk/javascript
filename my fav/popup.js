0201ab11ffc63f5df6416e1c6e245618ab4d314e
nobertk <nobertkbarugahare@gmail.com>
2017-06-02 13:41:23 +0300

    Add files via upload

---
+++ my fav/popup.js
@@ -0,0 +1,10 @@
+function loadUrls() {
+	//copy url from text area  
+}
+
+function saveUrls() {
+	// save url in chrome stroge
+}
+
+document.getElementByld('button').addEventLitener('click', loadUrls);
+document.getElementByld('button').addEventLitener('click', saveUrls)
